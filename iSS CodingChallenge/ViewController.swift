b1c03401f857e03e1919c54dec0e2a2b595ce81a
Tam Nguyen <tamnguyen@tams-macbook-air.local>
2018-01-23 12:30:49 -0600

    Initial Commit

---
+++ iSS CodingChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSS CodingChallenge
+//
+//  Created by Tam Nguyen on 1/23/18.
+//  Copyright © 2018 Tam Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
